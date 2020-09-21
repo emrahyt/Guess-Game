b3e1b05f43be5dfca0aa892bb4c31e13fd03c54a
emrahyasart <emrah35@gmail.com>
2020-09-21 06:43:22 +0300

    Fİrst commit

---
+++ constants/default-styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  bodyText: {
+    fontFamily: "open-sans",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 18,
+  },
+});
+
+//This file is alternative for creating components like BodyText or TitleText
